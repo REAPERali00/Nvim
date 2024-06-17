618f70b80380f5a17de80f32ac065a748e04d923
REAPERali00 <alireza2000jadidi@gmail.com>
2024-06-17 13:01:12 -0400

    fixed harpoon add issue, removed integrations

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,7 +9,7 @@ return {
     {
       "<leader>H",
       function()
-        require("harpoon"):list():append()
+        require("harpoon"):list():add()
       end,
       desc = "harpoon file",
     },
