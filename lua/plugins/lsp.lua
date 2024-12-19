d037e2ecb153c1d0a30ed17b9c826045077262f8
Alireza <alireza2000jadidi@gmail.com>
2024-12-19 13:06:12 -0500

    brought nvim cmp back

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -130,15 +130,8 @@ return {
       setup = {},
     },
   },
-  -- {
-  --   "nvim-cmp",
-  --   dependencies = { "hrsh7th/cmp-emoji" },
-  --   opts = function(_, opts)
-  --     table.insert(opts.sources, { name = "emoji" })
-  --   end,
-  -- },
   {
-    "blink.cmp",
+    "nvim-cmp",
     dependencies = { "hrsh7th/cmp-emoji" },
     opts = function(_, opts)
       table.insert(opts.sources, { name = "emoji" })
