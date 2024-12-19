494a8e605a3931ca0f5f22de677a817258c83e6c
Alireza <alireza2000jadidi@gmail.com>
2024-12-19 13:21:12 -0500

    added colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,22 @@
 return {
   {
-    "sainnhe/sonokai",
-    priority = 1000,
-    config = function()
-      vim.g.sonokai_transparent_background = "1"
-      vim.g.sonokai_enable_italic = "1"
-      vim.g.sonokai_style = "andromeda"
-      -- vim.cmd.colorscheme("sonokai")
-    end,
-  },
-  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
-  {
+    { "rebelot/kanagawa.nvim" },
+    { "rose-pine/neovim", name = "rose-pine" },
+    { "sainnhe/everforest" },
+    { "bluz71/vim-nightfly-colors" },
+    { "NLKNguyen/papercolor-theme" },
+    { "romainl/Apprentice" },
+    {
+      "sainnhe/sonokai",
+      priority = 1000,
+      config = function()
+        vim.g.sonokai_transparent_background = "1"
+        vim.g.sonokai_enable_italic = "1"
+        vim.g.sonokai_style = "andromeda"
+        -- vim.cmd.colorscheme("sonokai")
+      end,
+    },
+    { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = { transparent_background = true } },
     {
       "catppuccin",
       opts = {
@@ -20,7 +26,8 @@ return {
     {
       "LazyVim/LazyVim",
       opts = {
-        colorscheme = "catppuccin",
+        colorscheme = "habamax",
+        transparent_background = true,
       },
     },
   },
