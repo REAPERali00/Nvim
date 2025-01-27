24f0d1a135923e3962165bff6c4c2511aa5763cf
Alireza <alireza2000jadidi@gmail.com>
2025-01-26 19:50:52 -0500

    modified color schemes, add transparency for many colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,68 @@
 return {
   {
     { "rebelot/kanagawa.nvim" },
-    { "rose-pine/neovim", name = "rose-pine" },
+    {
+      "rose-pine/neovim",
+      priority = 1000,
+      config = function()
+        require("rose-pine").setup({
+          variant = "moon", -- auto, main, moon, or dawn
+          dark_variant = "moon", -- main, moon, or dawn
+          dim_inactive_windows = false,
+          extend_background_behind_borders = true,
+
+          enable = {
+            terminal = true,
+            legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+            migrations = true, -- Handle deprecated options automatically
+          },
+
+          styles = {
+            bold = true,
+            italic = true,
+            transparency = true,
+          },
+
+          groups = {
+            border = "muted",
+            link = "iris",
+            panel = "surface",
+
+            error = "love",
+            hint = "iris",
+            info = "foam",
+            note = "pine",
+            todo = "rose",
+            warn = "gold",
+
+            git_add = "foam",
+            git_change = "rose",
+            git_delete = "love",
+            git_dirty = "rose",
+            git_ignore = "muted",
+            git_merge = "iris",
+            git_rename = "pine",
+            git_stage = "iris",
+            git_text = "rose",
+            git_untracked = "subtle",
+
+            h1 = "iris",
+            h2 = "foam",
+            h3 = "rose",
+            h4 = "gold",
+            h5 = "pine",
+            h6 = "foam",
+          },
+
+          highlight_groups = {
+            -- Comment = { fg = "foam" },
+            -- VertSplit = { fg = "muted", bg = "muted" },
+            NormalFloat = { bg = "none" },
+          },
+        })
+        -- vim.cmd("colorscheme rose-pine-main")
+      end,
+    },
     {
       "neanias/everforest-nvim",
       config = function()
@@ -23,7 +84,19 @@ return {
         -- vim.cmd.colorscheme("sonokai")
       end,
     },
-    { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = { transparent_background = true } },
+    { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+    {
+      "folke/tokyonight.nvim",
+      lazy = false,
+      priority = 1000,
+      opts = {
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      },
+    },
     {
       "catppuccin",
       opts = {
@@ -33,8 +106,7 @@ return {
     {
       "LazyVim/LazyVim",
       opts = {
-        colorscheme = "everforest",
-        -- transparent_background = true,
+        colorscheme = "tokyonight",
       },
     },
   },
