fce71cc9d2fd6a3f6f7243bbbb241b1c51eb3b86
Alireza <alireza2000jadidi@gmail.com>
2025-02-03 16:42:31 -0500

    moving pyton lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,25 +1,40 @@
 return {
-  -- tools
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "luacheck",
-        "shellcheck",
-        "shfmt",
-        "tailwindcss-language-server",
-        "typescript-language-server",
-        "css-lsp",
-        "csharpier",
-        "netcoredbg",
-        "csharp-language-server",
-      })
-    end,
-  },
-
   -- lsp servers
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      -- Automatically install LSPs and related tools to stdpath for Neovim
+      -- Mason must be loaded before its dependents so we need to set it up here.
+      -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+      {
+        "williamboman/mason.nvim",
+        opts = {
+          opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, {
+              "black",
+              "ruff",
+              "luacheck",
+              "shellcheck",
+              "shfmt",
+              "tailwindcss-language-server",
+              "typescript-language-server",
+              "css-lsp",
+              "csharpier",
+              "netcoredbg",
+              "csharp-language-server",
+            })
+          end,
+        },
+      },
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+
+      -- Useful status updates for LSP.
+      { "j-hui/fidget.nvim", opts = {} },
+
+      -- Allows extra capabilities provided by nvim-cmp
+      "hrsh7th/cmp-nvim-lsp",
+    },
     opts = {
       inlay_hints = { enabled = true },
       ---@type lspconfig.options
@@ -29,6 +44,23 @@ return {
           root_dir = function(...)
             return require("lspconfig.util").root_pattern(".git")(...)
           end,
+        },
+        pylsp = {
+          mason = false,
+          settings = {
+            pylsp = {
+              plugins = {
+                rope_autoimport = {
+                  enabled = true,
+                },
+              },
+            },
+          },
+        },
+        ruff_lsp = {
+          -- handlers = {
+          --   ["textDocument/publishDiagnostics"] = function() end,
+          -- },
         },
         tsserver = {
           root_dir = function(...)
@@ -127,7 +159,24 @@ return {
           },
         },
       },
-      setup = {},
+      setup = {
+        ruff_lsp = function()
+          require("lazyvim.util").lsp.on_attach(function(client, _)
+            if client.name == "ruff_lsp" then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end)
+        end,
+        pyright = function()
+          require("lazyvim.util").lsp.on_attach(function(client, _)
+            if client.name == "pyright" then
+              -- disable hover in favor of jedi-language-server
+              client.server_capabilities.hoverProvider = false
+            end
+          end)
+        end,
+      },
     },
   },
   {
