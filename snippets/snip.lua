0a9187c99c3096230014e6a22870c8dcb6371f34
Alireza <alireza2000jadidi@gmail.com>
2025-07-09 10:55:11 -0400

    removed unnecessary pagckages

--- snippets/snip.lua
+++ snippets/snip.lua
@@ -3,26 +3,6 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
-ls.add_snippets("markdown", {
-  s("- explanation", {
-    t("- `"),
-    i(1, "code"),
-    t("` -> "),
-    i(2, "explanation"),
-  }),
-})
-
-ls.add_snippets("all", {
-  s("name", {
-    t("Alireza Jadidi"),
-  }),
-})
-ls.add_snippets("cs", {
-  s("crl", {
-    t("Console.ReadLine();"),
-  }),
-})
-
 ls.add_snippets("sh", {
   s("bash", {
     t("#!/bin/bash"),
