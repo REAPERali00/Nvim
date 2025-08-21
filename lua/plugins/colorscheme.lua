acc9326839b27b54c1e82e0df930e4f303c3aa0e
alireza <alireza2000jadidi@gmail.com>
2025-08-21 14:56:24 -0400

    added new color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -185,9 +185,29 @@ return {
       },
     },
     {
+      "e-ink-colorscheme/e-ink.nvim",
+      priority = 1000,
+      config = function()
+        require("e-ink").setup()
+        -- vim.cmd.colorscheme "e-ink"
+        -- choose light mode or dark mode
+        -- vim.opt.background = "dark"
+        -- vim.opt.background = "light"
+        --
+        -- or do
+        -- :set background=dark
+        -- :set background=light
+        --
+        local set_hl = vim.api.nvim_set_hl
+        local mono = require("e-ink.palette").mono()
+
+        set_hl(0, "Normal", { fg = mono[12], bg = "NONE" })
+      end,
+    },
+    {
       "LazyVim/LazyVim",
       opts = {
-        colorscheme = "catppuccin",
+        colorscheme = "e-ink",
       },
     },
   },
