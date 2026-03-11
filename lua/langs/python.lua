c368e936ea73ad901cd8928bcce9b5992487df77
alireza <alireza2000jadidi@gmail.com>
2026-03-11 12:37:43 +0100

    updt

--- lua/langs/python.lua
+++ lua/langs/python.lua
@@ -1,20 +1,20 @@
 return {
   -- Setup adapters as nvim-dap dependencies
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "mfussenegger/nvim-dap-python",
-      -- stylua: ignore
-      keys = {
-        { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method" },
-        { "<leader>dPc", function() require('dap-python').test_class() end,  desc = "Debug Class" },
-      },
-      config = function()
-        local path = require("mason-registry").get_package("debugpy"):get_install_path()
-        require("dap-python").setup(path .. "/venv/bin/python")
-      end,
-    },
-  },
+  -- {
+  --   "mfussenegger/nvim-dap",
+  --   dependencies = {
+  --     "mfussenegger/nvim-dap-python",
+  --     -- stylua: ignore
+  --     keys = {
+  --       { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method" },
+  --       { "<leader>dPc", function() require('dap-python').test_class() end,  desc = "Debug Class" },
+  --     },
+  --     config = function()
+  --       local path = require("mason-registry").get_package("debugpy"):get_install_path()
+  --       require("dap-python").setup(path .. "/venv/bin/python")
+  --     end,
+  --   },
+  -- },
 
   -- Add `python` debugger to mason DAP to auto-install
   -- Not absolutely necessary to declare adapter in `ensure_installed`, since `mason-nvim-dap`
@@ -22,16 +22,6 @@ return {
   -- that are are set up (via dap) but not yet installed. Might as well skip the lines below as
   -- a whole.
 
-  -- Add which-key namespace for Python debugging
-  {
-    "folke/which-key.nvim",
-    optional = true,
-    opts = {
-      spec = {
-        { "<leader>dP", group = "Python" },
-      },
-    },
-  },
 
   -- For selecting virtual envs
   {
